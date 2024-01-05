4ec06196ba798e1fade674dad62aafa1a5c63605
f69 <f69@w>
2024-01-05 21:27:01 +0300

    добавление галереи в golden тест с обращением к серверу за реальными данными

--- test/golden/real_data_golden_test.dart
+++ test/golden/real_data_golden_test.dart
@@ -1,4 +1,5 @@
 import 'package:cherdak/app.dart';
+import 'package:cherdak/service/common_providers.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -11,12 +12,25 @@ void main() async {
 
   group('Real data tests', () {
     testGoldens('show main page', (tester) async {
+      final container = ProviderContainer();
+
       await tester.pumpWidgetGoldenAndWait(
-          widget: const ProviderScope(child: App()));
+          widget: UncontrolledProviderScope(
+              container: container, child: const App()));
       await tester.pumpAndSettle();
       await tester.pumpAndSettle();
 
-      await multiScreenGolden(tester, 'real_data.main_page', autoHeight: true);
+      await multiScreenGolden(tester, 'real_data.home_page.main',
+          autoHeight: true);
+
+      await tester.runAsync(() async {
+        container.read(homeTabProvider.notifier).state = HomeTab.gallery;
+        await tester.pump();
+        await tester.waitAndPump();
+      });
+
+      await multiScreenGolden(tester, 'real_data.home_page.gallery',
+          overrideGoldenHeight: 3600);
     });
   });
 }
