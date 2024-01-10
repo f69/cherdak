c4d0fe11d1121c0a65bac0186ed98c4647c0a038
f69 <f69@w>
2024-01-10 14:04:37 +0300

    тест MainMenu

---
+++ test/unit/components/main_menu_test.dart
@@ -0,0 +1,24 @@
+import 'package:cherdak/app/app_colors.dart';
+import 'package:cherdak/components/menu/main_menu.dart';
+import 'package:cherdak/components/menu/main_menu_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../mocks/mock_app.dart';
+
+void main() {
+  group('Main menu', () {
+    testWidgets('shows only one item selected', (tester) async {
+      await tester.pumpWidget(mockApp(home: const MainMenu()).app);
+      await tester.pumpAndSettle();
+      expect(find.byType(MainMenuItem), findsWidgets);
+
+      final textWidgets = tester.widgetList<Text>(find.byType(Text));
+      expect(
+          textWidgets
+              .where((e) => e.style?.color == AppColors.textWhite)
+              .length,
+          equals(1));
+    });
+  });
+}
